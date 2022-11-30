3b7b02a07e87e33a4a89c037286deacd9413149d
Valery <zonovvaleraooo@gmail.com>
2022-11-30 13:08:19 +0700

    Added role

---
+++ manager/src/Model/User/UseCase/Role/Command.php
@@ -0,0 +1,21 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace App\Model\User\UseCase\Role;
+
+class Command
+{    
+    /**
+     * id
+     *
+     * @var string
+     */
+    public $id;    
+    /**
+     * role
+     *
+     * @var string
+     */
+    public $role;
+}
